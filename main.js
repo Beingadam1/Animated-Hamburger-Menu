2a43e90c0222f0985944a14ab1c17e21a515b8ec
unknown <adamlanre224@gmail.com>
2020-12-31 03:28:24 +0800

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// Select The DOM Items
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Set The Intial State
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach((items) => items.classList.add("show"));
+
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach((items) => items.classList.remove("show"));
+
+    showMenu = false;
+  }
+}
